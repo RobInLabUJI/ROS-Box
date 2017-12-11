dc11b27e6dda2500bf093eee1cc16dfcb01e255e
Enric Cervera <ecervera@uji.es>
2017-12-11 14:53:57 +0100

    set path relative to HOME

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -199,7 +199,8 @@ c.NotebookApp.ip = '*'
 #c.NotebookApp.nbserver_extensions = {}
 
 ## The directory to use for notebooks and kernels.
-c.NotebookApp.notebook_dir = u'/home/user/ROS_Notebooks'
+import os
+c.NotebookApp.notebook_dir = os.environ['HOME'] + u'/ROS-Notebooks'
 
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
